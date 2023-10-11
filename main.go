b46f79cb3381b671dc8eeacbc22e82d8eb885fae
mofnnn <147251931+mofnnn@users.noreply.github.com>
2023-10-11 09:50:47 +0800

    Update main.go

--- main.go
+++ main.go
@@ -259,7 +259,7 @@ func init() {
 
 	// 通过代码写死的方式添加主人账号
 	// sus = append(sus, 1409618578)
-	// sus = append(sus, 87654321)
+	// sus = append(sus, 2339202441)
 
 	// 启用 webui
 	// go webctrl.RunGui(*g)
